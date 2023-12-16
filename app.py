25a701cfbd51bb977a0baf084a3afd4fb0d2886d
kevfo <75442525+kevfo@users.noreply.github.com>
2023-12-16 14:00:15 +0800

    Removed previous code and path handlers

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template
 
 # Import custom modules here:
-from utils.javascript import render_taxonium
-import json
+from utils.javascript import create_metadata, render_taxonium
 
 app = Flask(__name__)
 
@@ -10,81 +9,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/species_tree', methods = ['GET'])
-def species_tree():
-    return render_template('species_tree.html')
-
 @app.route('/view/<genus>', methods = ['GET'])
 def view(genus):
+    '''
+    Change this handler later:
+    '''
     raw_data, file_name = None, 'data/'
-    if genus == 'r':
-        file_name += 'test.jsonl'
-    elif genus == 'g':
-        file_name += 'test.jsonl'
-    elif genus == 'v':
-        file_name += 'test.jsonl'
-    with open(file_name) as data:
-        raw_data = '\n'.join(data.readlines())
-    print(raw_data)
-    return render_template('view.html', js_code = render_taxonium(genus, raw_data))
-
-
-'''
-@app.route('/search', methods = ['GET'])
-def search():
-    return render_template('search.html')
-
-@app.route('/entity/<ent>', methods = ['GET'])
-def entity(ent):
-    \'''
-    Given an 'ent', fetch information about it and display it on a 
-    separate page. 
-    \'''
-    entity_info = find_entity_information(ent)
-    return render_template('entity_page.html', info = entity_info)
-
-# INTERNAL ROUTES - MEANT TO BE ACCESSED BY THE APPLICATION, NOT THE USER
-
-@app.route('/find', methods = ['POST'])
-def find():
-    \'''
-    Do we still need this route (i.e., for that search bar page)?
-    \'''
-    query = find_queries(request.data.decode('utf-8'))
-    return jsonify(query)
-
-@app.route('/find_information/<item>', methods = ['POST'])
-def find_information(item):
-    \'''
-    Returns number of missing entities and also text color.
-    \'''
-    req = item.lower()
-    return jsonify({'text_color' : get_color(req), 
-                    'missing' : find_entity_information(req),
-                    'is_species' : is_species(req)})
-    
-@app.route('/populate_item/<item>', methods = ['GET', 'POST'])
-def populate_item(item):
-    \'''
-    Given an item, try to find its nested species.
-    \'''
-    req = item.split('(')[0].strip().lower()
-    return jsonify({'species' : find_species(req)})
-
-
-
-
-
-# Not sure if still need these routes...
-
-@app.route('/find_entity', methods = ['POST'])
-def find_entity():
-    query = find_entity_information(request.data.decode('utf-8'))
-    return jsonify(query)
-
-@app.route('/find_color/', methods = ['POST'])
-def find_color():
-    query = ''.join([i for i in request.data.decode('utf-8').split()[-1] if not i.isdigit()]).lower()
-    print(query, get_color(query))
-    return jsonify(get_color(query))
-'''
+    if genus.lower() == 'g':
+        file_name += 'G_taxon_fulllist.nw' 
+    elif genus.lower() == 'r':
+        file_name += 'R_taxonfull.nw'
+    elif genus.lower() == 'v':
+        file_name += 'V_taxonfull.nw'
+    raw_data = open(file_name).readline()
+    return render_template('view.html', meta = create_metadata(raw_data, file_name), render = render_taxonium())
